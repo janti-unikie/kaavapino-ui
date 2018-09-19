bf5b5d6538e236ad89468231d707b34df2fbb5fc
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-09-19 17:21:08 +0300

    Trying to make first Travis container image

---
+++ deploy/containerize.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$TRAVIS_NODE_VERSION" != "lts/*" ]; then
+    echo "Only deploy on production Python build"
+    exit 0
+fi
+
+export ORGANIZATION="helsinki"
+
+export IMAGE="kaavapino-ui"
+
+export REPO="$ORGANIZATION/$IMAGE"
+
+export COMMIT=${TRAVIS_COMMIT::7}
+
+export BRANCH=${TRAVIS_BRANCH//\//_}
+
+echo "Building image"
+docker build -t $IMAGE .
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+if [ "$TRAVIS_PULL_REQUEST" != false ]; then
+    echo "Tagging pull request" "$TRAVIS_PULL_REQUEST"
+    export BASE="$REPO:pr-$TRAVIS_PULL_REQUEST"
+    docker tag "$IMAGE" "$BASE"
+    docker tag "$BASE" "$REPO-$TRAVIS_BUILD_NUMBER"
+    docker push "$BASE"
+    docker push "$REPO:travis-$TRAVIS_BUILD_NUMBER"
+    exit 0
+fi
+
+echo "Tagging branch " "$TRAVIS_BRANCH"
+docker tag "$IMAGE" "$REPO:$COMMIT"
+docker tag "$REPO:$COMMIT" "$REPO:$BRANCH"
+docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER"
+docker push "$REPO:$COMMIT"
+docker push "$REPO:travis-$TRAVIS_BUILD_NUMBER"
+docker push "$REPO:$BRANCH"
+exit 0
