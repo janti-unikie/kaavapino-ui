a8e7478f87d1f99399a5ff20557a92a6cc27d89e
woltsu <oltsuwh@gmail.com>
2018-12-12 12:12:58 +0200

    Added sentry and initialization of sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { OidcProvider, processSilentRenew } from 'redux-oidc'
+import { init as sentryInit } from '@sentry/browser'
 import App from './components/App'
 import store from './store'
 import userManager from './utils/userManager'
@@ -18,6 +19,11 @@ if (window.location.pathname === '/silent-renew') {
 
   // Initialize icons
   iconHandler.initIcons()
+
+  // Initialize sentry
+  if (process.env.NODE_ENV === 'production') {
+    sentryInit({ dsn: process.env.REACT_APP_SENTRY_URL })
+  }
 
   ReactDOM.render(
     <Provider store={store}>
