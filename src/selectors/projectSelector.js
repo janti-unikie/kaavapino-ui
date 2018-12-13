ad494bd18ec8e8be52cefff40a6d26f1ef69ef59
woltsu <oltsuwh@gmail.com>
2018-12-13 14:22:36 +0200

    Added info about latest field edit to project edit page

--- src/selectors/projectSelector.js
+++ src/selectors/projectSelector.js
@@ -14,7 +14,7 @@ export const ownProjectsSelector = createSelector(
 
 export const currentProjectSelector = createSelector(
   selectProject,
-  (project) => project.currentProject
+  (project) => project.currentProject || null
 )
 
 export const attributeDataSelector = createSelector(
@@ -59,5 +59,20 @@ export const checkingSelector = createSelector(
 
 export const updatesSelector = createSelector(
   currentProjectSelector,
-  ({ _metadata }) => _metadata ? _metadata.updates ? _metadata.updates : {} : {}
+  (currentProject = {}) => currentProject ? currentProject._metadata ? currentProject._metadata.updates ? currentProject._metadata.updates : {} : {} : {}
+)
+
+export const latestUpdateSelector = createSelector(
+  updatesSelector,
+  (updates) => {
+    let latest = null
+    let field = null
+    Object.keys(updates).forEach((key) => {
+      if (!latest || (new Date(updates[key].timestamp) - new Date(latest.timestamp) > 0)) {
+        latest = updates[key]
+        field = key
+      }
+    })
+    return { latest, field }
+  }
 )
