0761319be2be4191a48ced4f5e6b36444d6c9658
Heikki Moilanen <heikki.moilanen@codemate.com>
2020-07-09 10:55:15 +0300

    shoutbox should be a functional component and use hooks instead of a class

--- src/components/shoutbox/index.js
+++ src/components/shoutbox/index.js
@@ -1,38 +1,25 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Button } from 'semantic-ui-react'
 import './shoutbox.scss'
 import Comments from './comments'
 
-class Shoutbox extends Component {
-  constructor(props) {
-    super(props)
+const Shoutbox = (props) => {
+  const { project } = props
 
-    this.state = {
-      open: false,
-      newMessages: 0
-    }
-  }
+  const [open, setOpen] = useState(false)
+  const toggleOpen = () => setOpen(!open)
 
-  toggleOpen = () => {
-    this.setState({ open: !this.state.open })
-  };
-
-  render() {
-    const { open } = this.state
-    const { project } = this.props
-
-    return (
-      <div className={`shoutbox ${open ? 'open' : ''}`}>
-        <Button className='shoutbox-button' onClick={this.toggleOpen}>
-          <div>Viestit</div>
-          <div className="comment-icon" />
-        </Button>
-        <div className='comment-list-wrapper'>
-          <Comments project={project} />
-        </div>
+  return (
+    <div className={`shoutbox ${open ? 'open' : ''}`}>
+      <Button className='shoutbox-button' onClick={toggleOpen}>
+        <div>Viestit</div>
+        <div className="comment-icon" />
+      </Button>
+      <div className='comment-list-wrapper'>
+        <Comments project={project} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Shoutbox
