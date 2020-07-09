f1eb8ec25cb8dc5682bed99cc256e1dc298b39bf
Heikki Moilanen <heikki.moilanen@codemate.com>
2020-07-09 11:43:03 +0300

    handling outside clicks should be a hook for generic usability. Shoutbox should close on outside click

--- src/components/shoutbox/index.js
+++ src/components/shoutbox/index.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Button } from 'semantic-ui-react'
 import './shoutbox.scss'
 import Comments from './comments'
+import { useOutsideClick } from '../../hooks/useOutsideClick'
 
 const Shoutbox = (props) => {
   const { project } = props
 
   const [open, setOpen] = useState(false)
+  const shoutboxRef = useRef(null)
   const toggleOpen = () => setOpen(!open)
+  const handleOutsideClick = () => {
+    if (open) {
+      toggleOpen()
+    }
+  }
+
+  useOutsideClick(shoutboxRef, handleOutsideClick)
 
   return (
-    <div className={`shoutbox ${open ? 'open' : ''}`}>
+    <div className={`shoutbox ${open ? 'open' : ''}`} ref={shoutboxRef}>
       <Button className='shoutbox-button' onClick={toggleOpen}>
         <div>Viestit</div>
         <div className="comment-icon" />
