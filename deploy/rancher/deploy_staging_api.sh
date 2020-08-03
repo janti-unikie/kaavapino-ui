54176f7376752005fd02417bcf7d0fba471d065a
Kaarle Hovila <kaarle.hovila@unikie.com>
2020-08-03 17:35:04 +0300

    fixed staging deployment script

--- deploy/rancher/deploy_staging_api.sh
+++ deploy/rancher/deploy_staging_api.sh
@@ -12,13 +12,13 @@ fi
 VERSION=$1
 RUN=$2
 NS="kaavapino-staging"
-REGISTRY="127.0.0.1:34156"
+REGISTRY="127.0.0.1:34754"
 
 
 
 TMP=$(mktemp)
 echo "Using temporary file $TMP"
-cat deployment.yaml | sed 's,${CICD_REGISTRY},'$REGISTRY',g' | sed 's,${CICD_EXECUTION_SEQUENCE}-${CICD_GIT_COMMIT},'$VERSION',g' > $TMP
+cat deployment.yaml | sed 's,${CICD_IMAGE}:,'$REGISTRY/',g' | sed 's,${CICD_EXECUTION_SEQUENCE}-${CICD_GIT_COMMIT},'$VERSION',g' > $TMP
 cat $TMP
 KUBECTL="kubectl -n $NS apply -f $TMP"
 if test $RUN == "run"; then
