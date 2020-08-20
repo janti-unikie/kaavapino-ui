4030982830bf791a7e140f41fb1c268fb9e81842
Teemu Reuna <teemu.reuna@unikie.com>
2020-08-20 14:51:21 +0300

    Test finally working, added value to checkbox's

--- src/components/input/DropdownMultiselect.js
+++ src/components/input/DropdownMultiselect.js
@@ -4,46 +4,47 @@ import { Checkbox ,Dropdown } from 'semantic-ui-react'
 import inputUtils from '../../utils/inputUtils'
 
 const DropdownMultiselect = ({ input, error, options, ...custom }) => {
-    return (
-      <Dropdown
-        {...input}
-        {...custom}
-        fluid
-        search
-        clearable
-        placeholder=""
-        multiple
-        selection
-        options={options}
-        noResultsMessage="Ei tuloksia"
+  return (
+    <Dropdown
+      {...input}
+      {...custom}
+      fluid
+      search
+      clearable
+      placeholder=""
+      multiple
+      selection
+      options={options}
+      noResultsMessage="Ei tuloksia"
 
-        error={inputUtils.hasError(error)}
-        onBlur={() => input.onBlur(input.value.value)}
-        trigger={trigger(input.value)}
-      >
-        <Dropdown.Menu>
-          {options.map(({ key, text }) => {
-            return(
-              <Dropdown.Item key={key}>
-                <Checkbox
-                  className='dropdown-checkbox'
-                  label={text}
-                  onChange={(param, data) => {
-                    if (input.value.indexOf(data.label) !== -1) {
-                      const nonSelectedValues = input.value.filter( opt => opt !== data.label )
-                      input.onChange(nonSelectedValues)
-                    } else {
-                      input.onChange([...input.value, data.label])
-                    }
-                    }}
-                  checked={input.value.indexOf(text) !== -1}
-                />
-              </Dropdown.Item>
-            )
-          })}
-        </Dropdown.Menu>
-      </Dropdown>
-    )
+      error={inputUtils.hasError(error)}
+      onBlur={() => input.onBlur(input.value.value)}
+      trigger={trigger(input.value)}
+    >
+      <Dropdown.Menu>
+        {options.map(({ key, text, value }) => {
+          return(
+            <Dropdown.Item key={key}>
+              <Checkbox
+                className='dropdown-checkbox'
+                label={text}
+                onChange={(param, data) => {
+                  if (input.value.indexOf(data.label) !== -1) {
+                    const nonSelectedValues = input.value.filter( opt => opt !== data.label )
+                    input.onChange(nonSelectedValues)
+                  } else {
+                    input.onChange([...input.value, data.label])
+                  }
+                  }}
+                checked={input.value.indexOf(text) !== -1}
+                value={value}
+              />
+            </Dropdown.Item>
+          )
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  )
 }
 
 const trigger = (value) => {
