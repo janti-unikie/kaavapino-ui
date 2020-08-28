9378bff0c66738351fced193b5a2631d05272e95
Heikki Moilanen <heikki.moilanen@codemate.com>
2020-08-28 14:47:58 +0300

    remove FE-generated total fields, since they come from BE after all

--- src/components/input/Matrix/index.js
+++ src/components/input/Matrix/index.js
@@ -7,75 +7,8 @@ import './styles.scss'
 
 const TOTAL_FIELD_TYPE = 'totalField'
 
-/* Adds a total column at the right end of a matrix field and a total row at the bottom of it */
-const addTotalFields = (
-  { rows: dataRows, columns: dataColumns, fields: dataFields, ...rest },
-  attributeData
-) => {
-  const rows = [...dataRows, 'Yhteensä']
-  const columns = [...dataColumns, 'Yhteensä']
-
-  /* The unit information is in the fields, but all should be of the same unit */
-  const unit = dataFields && dataFields.length > 0 && dataFields[0].unit
-
-  /* Make a total field for each row */
-  const rowTotalFields = rows.map((row, i) => {
-    const rowFields = dataFields.filter(field => field.row === i)
-    const rowTotal = rowFields.reduce((total, current) => {
-      const val = (attributeData && attributeData[current.name]) || 0
-      return val ? total + val : total
-    }, 0)
-    const rowTotalField = {
-      type: TOTAL_FIELD_TYPE,
-      value: rowTotal ? `${rowTotal} ${unit}` : 0,
-      row: i,
-      column: dataColumns.length
-    }
-    return rowTotalField
-  })
-
-  /* Make a total field for each column */
-  const columnTotalFields = columns.map((col, i) => {
-    const colFields = dataFields.filter(field => field.column === i)
-    const colTotal = colFields.reduce((total, current) => {
-      const val = (attributeData && attributeData[current.name]) || 0
-      return val ? total + val : total
-    }, 0)
-    const columnTotalField = {
-      type: TOTAL_FIELD_TYPE,
-      value: colTotal ? `${colTotal} ${unit}` : 0,
-      row: dataRows.length,
-      column: i
-    }
-    return columnTotalField
-  })
-
-  /* Make a total of all fields to the right bottom spot */
-  const allTotalValue = dataFields.reduce((total, current) => {
-    const val = (attributeData && attributeData[current.name]) || 0
-    return val ? total + val : total
-  }, 0)
-  const allTotalField = {
-    type: TOTAL_FIELD_TYPE,
-    value: allTotalValue ? `${allTotalValue} ${unit}` : 0,
-    row: dataRows.length,
-    column: dataColumns.length
-  }
-
-  const fields = [...dataFields, ...columnTotalFields, ...rowTotalFields, allTotalField]
-
-  return {
-    rows,
-    columns,
-    fields,
-    ...rest
-  }
-}
-
 const Matrix = ({ field: { matrix }, checking, attributeData }) => {
   const { rows, columns, fields } = matrix
-    ? addTotalFields(matrix, attributeData)
-    : matrix
 
   const matrixStyle = {
     display: 'grid',
@@ -105,7 +38,6 @@ const Matrix = ({ field: { matrix }, checking, attributeData }) => {
                 return (
                   <React.Fragment key={`${field.name}-${y}-${x}-total`}>
                     <span style={{ display: 'contents' }}>
-                      {/* {x === 0 && <b>{rows[y]}</b>} */}
                       {field === 0 && <span />}
                       {field && field.type === TOTAL_FIELD_TYPE && (
                         <span className="matrix-total-field">
