28035ca59fd7e71a96223fcacfc6b4f3a6d38402
Jani Timmerheid <jani.timmerheid@unikie.com>
2020-12-21 09:35:02 +0200

    Changed component to function component

--- src/components/projectCard/index.js
+++ src/components/projectCard/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid, Segment } from 'semantic-ui-react'
 import BasicInformation from './BasicInformation'
 import Contract from './Contract'
@@ -14,43 +14,35 @@ import { fieldsMockData } from './fieldsMockData'
 import projectUtils from '../../utils/projectUtils'
 import GeometryInformation from './GeometryInformation'
 
-class ProjectCardPage extends PureComponent {
+function ProjectCardPage( { attributeData, deadlines } ) {
 
-  state = {
-    descriptionFields: [],
-    basicInformationFields: [],
-    contactsFields: [],
-    photoField:null,
-    strategyConnectionFields: [],
-    timeTableFields: [],
-    floorAreaFields: [],
-    contractFields: [],
-    documentFields: [],
-    planningRestriction: null
-  }
+  const [descriptionFields, setDescriptionDFields  ] = useState([])
+  const [ basicInformationFields, setBasicInformationFields ] = useState([])
+  const [contactsFields, setContactsFields  ] = useState([])
+  const [ photoField, setPhotoField ] = useState( null )
+  const [ strategyConnectionFields, setStrategyConnectionFields ] = useState([])
+  const [ timeTableFields, setTimeTableFields ] = useState([])
+  const [ floorAreaFields, setFloorAreaFields ] = useState([])
+  const [ contractFields, setContractFields ] = useState([])
+  const [ documentFields, setDocumentFields ] = useState([])
+  const [ planningRestriction, setPlanningRestriction ] = useState(null)
 
-  componentDidMount() {
-    const { projectTypes } = this.props
-    if (projectTypes) {
-      this.updateMetadata()
-    }
-    this.buildPage()
+  useEffect(() => {
+      buildPage()
+    }, [])
 
-  }
+  const buildPage = () => {
 
-  buildPage = () => {
-    const { attributeData } = this.props
-
-    const descriptionFields = []
-    const basicInformationFields= []
-    const contactsFields = []
-    let photoField = null
-    const strategyConnectionFields = []
-    const timeTableFields = []
-    const floorAreaFields = []
-    const contractFields = []
-    const documentFields = []
-    let planningRestriction = null
+    const currentDescriptionFields = []
+    const currentBasicInformationFields= []
+    const currentContactsFields = []
+    let currentPhotoField = null
+    const currentStrategyConnectionFields = []
+    const currentTimeTableFields = []
+    const currentFloorAreaFields = []
+    const currentContractFields = []
+    const currentDocumentFields = []
+    let currentPlanningRestriction = null
 
     fieldsMockData.forEach( field => {
       const value = projectUtils.findValueFromObject( attributeData, field.name )
@@ -66,65 +58,61 @@ class ProjectCardPage extends PureComponent {
           description: value === undefined ? null :  value.description
 
         }
-       photoField = newField
+       currentPhotoField = newField
       }
       if ( field.display === 'basic') {
-
-          basicInformationFields.push( newField )
+        currentBasicInformationFields.push( newField )
       }
       if ( field.display === 'description') {
-        descriptionFields.push( newField )
+        currentDescriptionFields.push( newField )
       }
       if ( field.display === 'strategy') {
-        strategyConnectionFields.push( newField )
+        currentStrategyConnectionFields.push( newField )
       }
       if ( field.display === 'contract') {
-        contractFields.push( newField )
+        currentContractFields.push( newField )
       }
       if ( field.display === 'floor-area-information') {
-       floorAreaFields.push( newField )
+        currentFloorAreaFields.push( newField )
       }
       if ( field.display === 'timetable') {
-       timeTableFields.push( newField )
+        currentTimeTableFields.push( newField )
       }
       if ( field.display === 'contact') {
-        contactsFields.push( newField )
+        currentContactsFields.push( newField )
       }
       if ( field.display === 'documents') {
-        documentFields.push( newField )
+        currentDocumentFields.push( newField )
       }
-
       if ( field.display === 'geometry') {
-          planningRestriction = newField
-        }
+        currentPlanningRestriction = newField
+      }
      })
 
-     this.setState( {
-      descriptionFields,
-      basicInformationFields,
-      contactsFields,
-      photoField,
-      strategyConnectionFields,
-      timeTableFields,
-      floorAreaFields,
-      contractFields,
-      documentFields,
-      planningRestriction
-     })
+     setDescriptionDFields(currentDescriptionFields)
+     setBasicInformationFields(currentBasicInformationFields)
+     setContactsFields(currentContactsFields)
+     setPhotoField( currentPhotoField )
+     setStrategyConnectionFields( currentStrategyConnectionFields )
+     setTimeTableFields( currentTimeTableFields )
+     setFloorAreaFields( currentFloorAreaFields )
+     setContractFields( currentContractFields)
+     setDocumentFields( currentDocumentFields )
+     setPlanningRestriction( currentPlanningRestriction )
+
   }
 
-  renderFirstRow = () => (
+  const renderFirstRow = () => (
     <Grid stackable columns='equal'>
         <Grid.Column width={8}>
-          <Segment><Description fields={this.state.descriptionFields}/></Segment>
+          <Segment><Description fields={descriptionFields}/></Segment>
         </Grid.Column>
         <Grid.Column>
-          <Segment><Photo field={this.state.photoField} /></Segment>
+          <Segment><Photo field={photoField} /></Segment>
         </Grid.Column>
     </Grid>
   )
-  renderTimeLineRow = () => {
-    const { deadlines } = this.props
+  const renderTimeLineRow = () => {
 
     return (
       <Grid stackable columns='equal'>
@@ -134,61 +122,58 @@ class ProjectCardPage extends PureComponent {
       </Grid>
     )
   }
-  renderSecondRow = () => {
+  const renderSecondRow = () => {
 
     return (
       <Grid stackable columns='equal'>
           <Grid.Column width={5}>
             <Segment>
-              <Contacts fields={this.state.contactsFields}/>
+              <Contacts fields={contactsFields}/>
             </Segment>
             <Segment key="basic-information">
-              <StrategyConnection fields={this.state.strategyConnectionFields}/>
+              <StrategyConnection fields={strategyConnectionFields}/>
           </Segment>
             <Segment>
-                <TimeTable fields={this.state.timeTableFields}/>
+                <TimeTable fields={timeTableFields}/>
             </Segment>
           </Grid.Column>
           <Grid.Column>
             <Segment>
-              <FloorAreaInformation fields={this.state.floorAreaFields} />
+              <FloorAreaInformation fields={floorAreaFields} />
             </Segment>
             <Grid columns='equal'>
               <Grid.Column className="inner-left-column">
                 <Segment key="basic-information">
-                  <BasicInformation fields={this.state.basicInformationFields} />
+                  <BasicInformation fields={basicInformationFields} />
                 </Segment>
               </Grid.Column>
               <Grid.Column className="inner-right-column">
                 <Segment>
-                  <Contract fields={this.state.contractFields} />
+                  <Contract fields={contractFields} />
                 </Segment>
               </Grid.Column>
             </Grid>
             <Segment>
-              <GeometryInformation field={this.state.planningRestriction} />
+              <GeometryInformation field={planningRestriction} />
             </Segment>
             <Segment>
-              <Documents fields={this.state.documentFields} />
+              <Documents fields={documentFields} />
             </Segment>
           </Grid.Column>
       </Grid>
     )
   }
+  const firstRow = renderFirstRow()
+  const secondRow = renderSecondRow()
+  const timelineRow = renderTimeLineRow()
 
-  render() {
-    const firstRow = this.renderFirstRow()
-    const secondRow = this.renderSecondRow()
-    const timelineRow = this.renderTimeLineRow()
-
-    return (
-      <div className="project-card">
-          {firstRow}
-          {timelineRow}
-          {secondRow}
-      </div>
-    )
-  }
+  return (
+    <div className="project-card">
+        {firstRow}
+        {timelineRow}
+        {secondRow}
+    </div>
+  )
 }
 
 export default ProjectCardPage
