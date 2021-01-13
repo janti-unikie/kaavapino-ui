8f3dab8cb403d75b9bedb46e2a45991a4c28e635
Jani Timmerheid <jani.timmerheid@unikie.com>
2021-01-13 12:36:08 +0200

    Removed console log. Some style fixes

--- src/components/input/CustomCheckbox.js
+++ src/components/input/CustomCheckbox.js
@@ -6,24 +6,23 @@ const CustomCheckbox = ({
   meta: { error },
   ...custom
 }) => {
-    const onChangeSave = (data) => {
-      onChange(data.checked)
-      if ( custom.handleSave ) {
-        custom.handleSave(data.checked)
-      }
+  const onChangeSave = data => {
+    onChange(data.checked)
+    if (custom.handleSave) {
+      custom.handleSave(data.checked)
     }
-    console.log( value )
+  }
   return (
-      <Checkbox
-       {...custom}
-        disabled={disabled}
-        label={label}
-        error={error}
-        name={name}
-        className={className}
-        onChange={(e, data) => onChangeSave(data)}
-        defaultChecked={value}
-      />
+    <Checkbox
+      {...custom}
+      disabled={disabled}
+      label={label}
+      error={error}
+      name={name}
+      className={className}
+      onChange={(e, data) => onChangeSave(data)}
+      defaultChecked={value}
+    />
   )
 }
 
