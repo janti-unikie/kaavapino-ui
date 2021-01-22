d064def634a181efeaf6b05ecd5b53cb272449e5
Jani Timmerheid <jani.timmerheid@unikie.com>
2021-01-22 11:36:01 +0200

    Fixed timeline modal

--- src/components/input/DeadlineInfoText.js
+++ src/components/input/DeadlineInfoText.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import moment from 'moment'
 import { getFormValues } from 'redux-form'
-import { EDIT_PROJECT_FORM } from '../../constants'
+import { EDIT_PROJECT_TIMETABLE_FORM } from '../../constants'
 import { getFieldAutofillValue } from '../../utils/projectAutofillUtils'
 import { useSelector } from 'react-redux'
 
 const DeadlineInfoText = props => {
-  const formValues = useSelector(getFormValues(EDIT_PROJECT_FORM))
+  const formValues = useSelector(getFormValues(EDIT_PROJECT_TIMETABLE_FORM))
   let inputValue = props.input && props.input.value
 
   if (props.autofillRule) {
@@ -14,7 +14,11 @@ const DeadlineInfoText = props => {
   }
 
   // Expect date in value
-  const value = inputValue && moment(inputValue).format('DD.MM.YYYY')
+  let value = inputValue && moment(inputValue).format('DD.MM.YYYY')
+  if (value==='Invalid date') {
+    value = inputValue
+  }
+
   return (
     <div className="deadline-info-text">
       {props.label} {value}
