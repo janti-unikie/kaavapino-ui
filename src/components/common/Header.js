0898bff3207076eb01ace4531a764cbfc998d2db
Jani Timmerheid <jani.timmerheid@unikie.com>
2021-03-25 12:43:50 +0200

    Fixed styles

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,12 +6,12 @@ import { ReactComponent as PagesMobile } from '../../assets/pages-mobile.svg'
 
 import { withRouter } from 'react-router-dom'
 
-const Header = props =>  {
- const navigateToProjects = () => {
+const Header = props => {
+  const navigateToProjects = () => {
     props.history.push('../projects')
   }
 
- const navigateToHome = () => {
+  const navigateToHome = () => {
     props.history.push('../')
   }
 
@@ -24,53 +24,69 @@ const Header = props =>  {
   }
   const { user } = props
 
-    return (
-      <Navigation
-        logoLanguage="fi"
-        menuToggleAriaLabel="Valikko"
-        searchLabel="Search"
-        searchPlaceholder="Search page"
-        skipTo="#content"
-        skipToContentLabel="Siirry sivun pääsisältöön"
-        title="Kaavapino"
-        titleAriaLabel="Helsinki: Kaavapino"
-        titleUrl="./"
-        className="header"
-      >
-        <Navigation.Row variant="inline">
+  return (
+    <Navigation
+      logoLanguage="fi"
+      menuToggleAriaLabel="Valikko"
+      searchLabel="Search"
+      searchPlaceholder="Search page"
+      skipTo="#content"
+      skipToContentLabel="Siirry sivun pääsisältöön"
+      title="Kaavapino"
+      titleAriaLabel="Helsinki: Kaavapino"
+      titleUrl="./"
+      className="header"
+      theme={{
+        '--header-background-color': 'var(--color-fog-light)',
+        '--header-color': 'var(--color-black-90)',
+        '--header-divider-color': 'var(--color-black-20)',
+        '--header-focus-outline-color': 'var(--color-black)',
+        '--mobile-menu-background-color': 'var(--color-white)',
+        '--mobile-menu-color': 'var(--color-black-90)',
+        '--navigation-row-background-color': 'var(--color-white)',
+        '--navigation-row-color': 'var(--color-black-90)',
+        '--navigation-row-focus-outline-color': 'var(--color-coat-of-arms)'
+      }}
+    >
+      <Navigation.Row variant="inline">
+        <Navigation.Item
+          as="a"
+          label="Yleisnäkymä"
+          onClick={navigateToHome}
+          icon={<HistogramMobileIcon />}
+        />
+        <Navigation.Item
+          as="a"
+          label="Projektit"
+          onClick={navigateToProjects}
+          icon={<ChecklistMobile />}
+        />
+        <Navigation.Item
+          as="a"
+          label="Raportit"
+          icon={<PagesMobile />}
+          onClick={navigateToReports}
+        />
+      </Navigation.Row>
+      <Navigation.Actions>
+        <Navigation.User authenticated={true}>
           <Navigation.Item
-            as="a"
-            label="Yleisnäkymä"
-            onClick={navigateToHome}
-            icon={<HistogramMobileIcon />}
+            label={user.profile.name}
+            href="/"
+            target="_blank"
+            variant="primary"
           />
           <Navigation.Item
-            as="a"
-            label="Projektit"
-            onClick={navigateToProjects}
-            icon={<ChecklistMobile />}
+            href="#"
+            icon={<IconSignout aria-hidden />}
+            label="Sign out"
+            onClick={logout}
+            variant="supplementary"
           />
-          <Navigation.Item
-            as="a"
-            label="Raportit"
-            icon={<PagesMobile />}
-            onClick={navigateToReports}
-          />
-        </Navigation.Row>
-        <Navigation.Actions>
-          <Navigation.User authenticated={true}>
-            <Navigation.Item label={user.profile.name} href="/" target="_blank" variant="primary" />
-            <Navigation.Item
-              href="#"
-              icon={<IconSignout aria-hidden />}
-              label="Sign out"
-              onClick={logout}
-              variant="supplementary"
-            />
-          </Navigation.User>
-        </Navigation.Actions>
-      </Navigation>
-    )
-  }
+        </Navigation.User>
+      </Navigation.Actions>
+    </Navigation>
+  )
+}
 
 export default withRouter(Header)
