de0a71a0c44e71316fd5fd7625dc19743c6a83e2
Jani Timmerheid <jani.timmerheid@unikie.com>
2021-09-20 17:27:34 +0300

    Fixed document edit scroll

--- src/components/input/FormField.js
+++ src/components/input/FormField.js
@@ -148,7 +148,7 @@ const FormField = props => {
       >
         {!isOneLineField && (
           <div className="input-header">
-            <Label className={`input-title${required ? ' highlight' : ''}`}>
+            <Label id={field.name} className={`input-title${required ? ' highlight' : ''}`}>
               {title}
             </Label>
             <div className="input-header-icons">
